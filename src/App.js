77be020f703fbd2b837620038f0249dea63ce450
buithanhlong1912 <buithanhlong1912@gmail.com>
2022-05-21 02:33:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const Hello = ({ props }) => {
+  return <h1>{props}</h1>;
+};
 
 function App() {
+  // Functional component
+  const [state, setState] = useState("Hello");
+  const [count, setCount] = useState(-1);
+  // useState, useEffect, useReducer
+  // useContext, useReducer, useRef, useCallback,...
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubtract = () => {
+    count <= 0 ? setCount(count) : setCount(count - 1);
+  };
+
+  useEffect(() => {
+    console.log("render");
+    // if (count <= 0) setCount(count);
+    // setCount(0);
+  });
+  // Dependencies array
+  // TH1: không có thằng [] => Chạy thằng effect mỗi khi mà component re-render
+  // TH2: [] => Chỉ chạy 1 lần đầu khi thằng component render
+  // TH3: [] có giá trị bên trong => Chạy thằng effect mỗi khi mà giá trị bên trong array thay đổi
+
   return (
+    // JSX: JavaScript XML
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Hello props={count} />
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        // className="App-link"
+        // href="https://reactjs.org"
+        // target="_blank"
+        // rel="noopener noreferrer"
         >
-          Learn React
+          Tran Manh Phong
         </a>
+        <button onClick={handleAdd}>Them</button>
+        <button onClick={handleSubtract}>Bot</button>
       </header>
     </div>
   );
 }
+
+// class App extends React.Component {
+//   compon
+// } // Class component
 
 export default App;
